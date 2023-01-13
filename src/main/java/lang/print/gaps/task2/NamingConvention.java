585c78c78b8783f35a91e556de8d16d75e50a860
Assetjan <asetziyabek@gmail.com>
2023-01-14 01:33:52 +0600

    Convention

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
-        int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        int VARIABLE = 10;
+        int VARIABLE_ONE = 1;
+        int VARIABLE_TWO = 2;
+        int VARIABLE_THREE = 3;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        int VARIABLE_SIX = 6;
+        int VARIABLE_SEVEN = 7;
+        int VARIABLE_EIGHT = 8;
     }
 }
